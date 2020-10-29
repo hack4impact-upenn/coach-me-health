10c948b38dd09d76b63d3421d6b09d72fd1c889c
Carol Li <caroldotli@gmail.com>
2020-10-29 13:59:54 -0400

    Add font styling

--- src/client/src/pages/Main.tsx
+++ src/client/src/pages/Main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
+import '../styles/main.css';
+
 
 const ContentContainer = styled.div`
   text-align: center;
@@ -20,29 +22,35 @@ const Main = () => {
   const history = useHistory();
 
   return (
+    // TODO: 
+    // MAKE PAGES
+    // UPDATE STYLING/MAKE UNIVERSAL STYLING/PRELIM DASBHOARD DESIGN
+    // <ContentContainer>
+    //   <h1 className="title is-1">Welcome to TS Boilerplate</h1>
+    //   <p>
+    //     Dead simple monorepo boilerplate web project designed to take you from
+    //     Git Cloning to Heroku Deployment in less 5 minutes. 🚀🤩
+    //   </p>
+    //   <a href="https://github.com/Orang-utan/ts-boilerplate">
+    //     Check out our Github
+    //   </a>
+    //   <ButtonGroup>
+    //     <Button
+    //       className="button is-primary"
+    //       onClick={() => history.push('/signup')}
+    //     >
+    //       Sign Up
+    //     </Button>
+    //     <Button
+    //       className="button is-secondary"
+    //       onClick={() => history.push('/login')}
+    //     >
+    //       Login
+    //     </Button>
+    //   </ButtonGroup>
+    // </ContentContainer>
     <ContentContainer>
-      <h1 className="title is-1">Welcome to TS Boilerplate</h1>
-      <p>
-        Dead simple monorepo boilerplate web project designed to take you from
-        Git Cloning to Heroku Deployment in less 5 minutes. 🚀🤩
-      </p>
-      <a href="https://github.com/Orang-utan/ts-boilerplate">
-        Check out our Github
-      </a>
-      <ButtonGroup>
-        <Button
-          className="button is-primary"
-          onClick={() => history.push('/signup')}
-        >
-          Sign Up
-        </Button>
-        <Button
-          className="button is-secondary"
-          onClick={() => history.push('/login')}
-        >
-          Login
-        </Button>
-      </ButtonGroup>
+      <h1>Welcome to the CoachMe Health patient dashboard!</h1>
     </ContentContainer>
   );
 };
