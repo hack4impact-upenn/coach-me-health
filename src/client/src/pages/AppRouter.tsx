40bb6dc5d26caaf1d18e7523d867b8fc848b7b5b
Jared Asch <jaredasch1@gmail.com>
2020-11-28 20:16:07 -0500

    patient dashboard

--- src/client/src/pages/AppRouter.tsx
+++ src/client/src/pages/AppRouter.tsx
@@ -4,6 +4,7 @@ import Signup from './Signup';
 import Login from './Login';
 import Dashboard from './Dashboard';
 import Profile from './Profile';
+import PatientDashboard from './PatientDashboard';
 import AddPatientForm from '../components/AddPatientForm';
 import AppContainer from '../components/AppContainer';
 import PrivateRoute from '../components/PrivateRoute';
@@ -22,6 +23,7 @@ const AppRouter = () => {
           <PrivateRoute exact path="/dashboard" component={Dashboard} />
           <PrivateRoute exact path="/patient_add" component={AddPatientForm} />
           <PrivateRoute exact path="/profile" component={Profile} />
+          <PrivateRoute exact path="/patients" component={PatientDashboard}/>
           <PublicRoute exact={false} path="/" component={Main} />
         </Switch>
       </AppContainer>
