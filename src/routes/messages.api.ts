7f84afe34209549240fb4ac1fa3ca8e0eec937f9
Matthew Dong <mattdong03@gmail.com>
2020-12-27 14:37:19 -0600

    stuff

--- src/routes/messages.api.ts
+++ src/routes/messages.api.ts
@@ -1,30 +1,22 @@
 /* eslint-disable no-shadow */
 import express from 'express';
+import { ObjectId } from 'mongodb';
 import { Message, IMessage } from '../models/message.model';
 import { Outcome, IOutcome } from '../models/outcome.model';
 import { Patient, IPatient } from '../models/patient.model';
 
 
-import auth from '../middleware/auth';
 import initializeScheduler from '../utils/scheduling';
-import secureAxios from '../../glacial-falls-14734/src/client/src/api/core/apiClient';
-import { update } from 'lodash';
 
 const router = express.Router();
 initializeScheduler();
-
-const updatePatient = (patient: any) => {
-  Patient.updateOne({_id: patient.patientID}, patient).then( () => {
-      return true;
-    });
-};
 
 router.post('/newMessage', async (req, res) => {
   // validate phone number
   if (!req.body.phoneNumber || req.body.phoneNumber.match(/\d/g) == null ||  req.body.phoneNumber.match(/\d/g).length !== 10){
     return res.status(400).json({
       msg: 'Unable to add message: invalid phone number'
-    });
+    }); 
   }
 
   if (!req.body.patientID || req.body.patientID == ''){
@@ -60,34 +52,6 @@ router.post('/newMessage', async (req, res) => {
       });
     });
   } 
-  const newMessage = new Message({
-    phoneNumber: req.body.phoneNumber,
-    patientID: req.body.patientID,
-    message: req.body.messsage,
-    sender: req.body.sender,
-    image: req.body.image,
-    date: req.body.date
-  });
-  return newMessage.save().then( () => {
-    // TODO Increase message count for patient ID message sent to depending on user
-    // done?
-    secureAxios.get(`/api/patients/getPatient/${newMessage.patientID}`).then( (res) => {
-      res.data.responseCount += 1;
-      const patient = new Patient({
-        firstName: res.data.firstName,
-        lastName: res.data.lastName,
-        language: res.data.language,
-        phoneNumber: res.data.phoneNumber,
-        reports: [],
-        responseCount: res.data.responseCount,
-        messagesSent: res.data.messagesSent,
-      });
-      updatePatient(patient);
-    });
-    res.status(200).json({
-      success: true
-    });
-  });
     
 });
 
@@ -123,19 +87,47 @@ router.post('/newOutcome', async (req, res) => {
   return newOutcome.save().then( () => {
     // TODO increase messages sent
     // done?
-    secureAxios.get(`/api/patients/getPatient/${newOutcome.patientID}`).then( (res) => {
-      res.data.messagesSent += 1;
-      const patient = new Patient({
-        firstName: res.data.firstName,
-        lastName: res.data.lastName,
-        language: res.data.language,
-        phoneNumber: res.data.phoneNumber,
-        reports: [],
-        responseCount: res.data.responseCount,
-        messagesSent: res.data.messagesSent,
-      });
-      updatePatient(patient);
+    Patient.findByIdAndUpdate(new ObjectId(req.body.patientId), { $inc: { responseCount : 1}});
+    res.status(200).json({
+      success: true
     });
+  });
+});
+
+router.post('/scheduledMessage', async (req, res) => {
+  // validate phone number
+  if (!req.body.phoneNumber || req.body.phoneNumber.match(/\d/g) == null ||  req.body.phoneNumber.match(/\d/g).length !== 10){
+    return res.status(400).json({
+      msg: 'Unable to add outcome: invalid phone number'
+    });
+  }
+
+  if (req.body.patientID == ''){
+    return res.status(400).json({
+      msg: 'Unable to add outcome: must include patient ID'
+    });
+  }
+
+  if (req.body.language == ''){
+    return res.status(400).json({
+      msg: 'Unable to add outcome: must include language'
+    });
+  }
+
+  const newMessage = new Message({
+    patientID: req.body.patientID,
+    phoneNumber: req.body.phoneNumber,
+    date: req.body.date,
+    response: req.body.response,
+    value: req.body.value,
+    alertType: req.body.alertType
+  });
+  return newMessage.save().then( () => {
+    // TODO increase messages sent
+    // done?
+
+    Patient.findByIdAndUpdate(new ObjectId(req.body.patientId), { $inc: { messagesSent : 1}});
+
     res.status(200).json({
       success: true
     });
--- src/utils/scheduling.ts
+++ src/utils/scheduling.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import schedule from 'node-schedule';
 import { Message, IMessage } from '../models/message.model';
 import { Outcome, IOutcome } from '../models/outcome.model';
-import schedule from 'node-schedule';
-import { accountSid, authToken } from "../keys/twilio";
+import { accountSid, authToken } from '../keys/twilio';
+import secureAxios from '../../glacial-falls-14734/src/client/src/api/core/apiClient';
+import { Patient } from '../models/patient.model';
+import { ObjectId } from 'mongodb';
 
 const twilio = require('twilio')(accountSid, authToken);
 
@@ -11,58 +14,78 @@ const schedulingInterval = 5;
 
 // selects all messages which should be sent within the next __ seconds, and schedules them to be sent
 const scheduleMessages = (interval : number) => {
-    // console.log("Scheduling messages to be sent...")
-    const intervalStart = new Date();
-    const intervalEnd = new Date(intervalStart.getTime());
-    intervalEnd.setSeconds(intervalEnd.getSeconds() + interval);
+  // console.log("Scheduling messages to be sent...")
+  const intervalStart = new Date();
+  const intervalEnd = new Date(intervalStart.getTime());
+  intervalEnd.setSeconds(intervalEnd.getSeconds() + interval);
 
-    const messages = Message.find({
-        date : {
-            $lt: intervalEnd,
-        },
-        sent: false
-    }, (err, docs) => {
-        docs.forEach( (doc) => {
-            console.log(`Scheduled to send message "${doc.message}" to ${doc.phoneNumber}`)
-            schedule.scheduleJob(doc.date, () => {
-                sendMessage(doc);
-            })
-        })
+  const messages = Message.find({
+    date : {
+      $lt: intervalEnd,
+    },
+    sent: false
+  }, (err, docs) => {
+    docs.forEach( (doc) => {
+      console.log(`Scheduled to send message "${doc.message}" to ${doc.phoneNumber}`);
+      schedule.scheduleJob(doc.date, () => {
+        sendMessage(doc);
+      });
+    });
+  });
+};
+
+const getPatientIdFromNumber = (number: any) => {
+  return Patient.findOne({ phoneNumber: number}).select('_id')
+    .then((patientId) => {
+      console.log(number);
+      if (!patientId) console.log('No patient found!');
+      return patientId;
     })
+    .catch((err) => { return (err.message);
+    });
 };
 
 // sends message, marks it as sent
 const sendMessage = (msg : IMessage) => {
-    console.log(`Sent message "${msg.message}" to ${msg.phoneNumber}`);
-    twilio.messages
-        .create({
-        body: msg.message,
-        from: '+14155286397', // this is hardcoded right now
-        to: msg.phoneNumber
-     });
- 
-    //         _    _                  _       
-    //        | |  | |                | |      
-    //        | |__| | _____      ____| |_   _ 
-    //        |  __  |/ _ \ \ /\ / / _` | | | |
-    //        | |  | | (_) \ V  V / (_| | |_| |
-    //        |_|  |_|\___/ \_/\_/ \__,_|\__, |
-    //                                    __/ |
-    //                                   |___/ 
-
-    // mark message as sent
-    Message.findOneAndUpdate( { _id: msg.id }, {
-        sent: true
-    }, (err, res) => {
-        if(err){
-           console.log(err)
-        }
+  console.log(`Sent message "${msg.message}" to ${msg.phoneNumber}`);
+  twilio.messages
+    .create({
+      body: msg.message,
+      from: '+14155286397', // this is hardcoded right now
+      to: msg.phoneNumber
     });
+ 
+  //         _    _                  _       
+  //        | |  | |                | |      
+  //        | |__| | _____      ____| |_   _ 
+  //        |  __  |/ _ \ \ /\ / / _` | | | |
+  //        | |  | | (_) \ V  V / (_| | |_| |
+  //        |_|  |_|\___/ \_/\_/ \__,_|\__, |
+  //                                    __/ |
+  //                                   |___/ 
+
+  // mark message as sent
+  Message.findOneAndUpdate( { _id: msg.id }, {
+    sent: true
+  }, (err, res) => {
+    if (err){
+      console.log(err);
+    }
+  });
+
+  // updates patient's sentmessages
+
+  const getId = getPatientIdFromNumber(msg.phoneNumber).then(
+    (id) => {
+      const patientId = new ObjectId(id._id);
+      Patient.findByIdAndUpdate(patientId, { $inc: { messagesSent : 1}});
+    });
+
 };
 
 const initializeScheduler = () => {
-    scheduleMessages(schedulingInterval);
-    setInterval(() => scheduleMessages(schedulingInterval), schedulingInterval * 1000);
-}
+  scheduleMessages(schedulingInterval);
+  setInterval(() => scheduleMessages(schedulingInterval), schedulingInterval * 1000);
+};
 
 export default initializeScheduler;
