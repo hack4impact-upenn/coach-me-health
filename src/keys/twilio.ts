60b794ec76a189c2f4b6199a593540c2034c7785
Ben Demers <ben.n.demers@gmail.com>
2020-12-28 02:02:06 -0500

    add phone #

--- src/keys/twilio.ts
+++ src/keys/twilio.ts
@@ -1,3 +1,4 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-export {accountSid, authToken};
+const twilioNumber = "(334) 781-4975";
+export {accountSid, authToken, twilioNumber};
